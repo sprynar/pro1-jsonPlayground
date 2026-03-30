192c245254f4ba197dce3cc5ab00f0de5940082b
sprynar <sprynar@zohomail.com>
2026-03-30 15:59:27 +0200

    po cvičení

---
+++ src/main/java/pro1/Task4/Recipe.java
@@ -0,0 +1,16 @@
+package pro1.Task4;
+
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private List<RecipeItem> recipeItems;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<RecipeItem> getRecipeItems() {
+        return recipeItems;
+    }
+}
